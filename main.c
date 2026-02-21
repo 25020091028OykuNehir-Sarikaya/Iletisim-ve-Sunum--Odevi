44b315b451424ce3b807c9e2924814b15acd9672
Öykü Nehir Sarıkaya <25020091028@ogr.istiklal.edu.tr>
2026-02-21 17:35:52 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    printf("İletişim ve Sunum Teknikleri Dersine Hoşgeldiniz\n");
+
+    return 0;
+}
